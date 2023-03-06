c9e9da9d4d0c6034a5d924c7d23ea2624154b1ca
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2023-03-06 17:03:01 +1100

    Апдейт с шопом

--- src/js/pages/Shop/Shop.vue
+++ src/js/pages/Shop/Shop.vue
@@ -5,4 +5,10 @@
   </div>
 
   <router-view />
+
+  <div class="mt-4 text-center white-and-shadow">
+    <h4 class="h6">Возникли вопросы по работе магазина? Вы можете задать их в нашем
+      <a href="/discord" class="text-white" target="_blank">Discord</a> или <a href="/" class="text-white">вернуться
+        на главную</a>.</h4>
+  </div>
 </template>
