39d73416345b2aba6da29838d89597bcabfb3964
Alexander Morozov <9192541+potatoru@users.noreply.github.com>
2024-05-08 12:15:45 +1100

    Legacy plugin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { resolve } from 'path'
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, splitVendorChunkPlugin, loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import legacy from '@vitejs/plugin-legacy'
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
-
   return defineConfig({
     plugins: [
       vue(),
-      // getSplitVendorChunkPlugin(process.env),
-      //getLegacyPlugin(process.env)
+      getLegacyPlugin(process.env),
     ],
 
     root: 'src/',
@@ -22,27 +20,38 @@ export default ({ mode }) => {
       outDir: resolve(__dirname, process.env.VITE_BUILD_PATH),
       emptyOutDir: true,
       manifest: false,
+      cssCodeSplit: false,
+
       rollupOptions: {
-        input: '/index.html'
+        input: '/index.html',
+        // output: {
+        //   // manualChunks (id) {
+        //   //   if (id.includes('node_modules')) {
+        //   //     return id.toString().split('node_modules/')[1].split(
+        //   //       '/')[0].toString();
+        //   //   }
+        //   // }
+        // }
       },
-      target: 'es2015',
+      //target: 'es2015',
       assetsInlineLimit: 4096,
       minify: true,
     },
 
     resolve: {
       alias: {
-        '@app': fileURLToPath(new URL('./src', import.meta.url))
+        '@app': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }
+    },
   })
 }
 
 function getLegacyPlugin (env) {
+  if (env.VITE_LEGACY_BUILD === true) {
     return legacy({
-      targets: 'defaults, android >= 4.4.4, ios >= 7',
-      externalSystemJS: true
+      targets: 'defaults, android >= 5.0, ios >= 8',
     })
+  }
 }
 
 // function getSplitVendorChunkPlugin (env) {
